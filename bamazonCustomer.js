e3574b6028218c2fc6183fec4e365a650146ebae
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-05-01 12:04:49 -0400

    Customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var isDone = false;
 var itemData;
-
+//Creates the mySQL connection 
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
@@ -10,31 +9,39 @@ var connection = mysql.createConnection({
 	password: 'Zazaza00!',
 	database: 'bamazon'
 });
+//Function for buying an item and udpating the database
 var buyItem = function(answer){
+	//The item picked by the user
 	var item = parseInt(answer.itemToBuy);
+	//the amount of items picked from the user
 	var amount = parseInt(answer.itemQuantity);
+	//Conditional if the user trys to buy too many items
 	if(itemData[item - 1].stock_quantity < amount){
 		console.log('Insufficient quantity!');
 		itemPrompt();
 	}
 	else{
+		//The amount of stock difference after the user buys the amount of items
 		var stockDiff = itemData[item-1].stock_quantity - amount;
+		//Updating the database with the new stock amount on the user selected item
 		connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [stockDiff, item]);
-		connection.query('SELECT * FROM products', function(err, res){
-			console.log('Total Cost: ' + (amount * parseFloat(itemData[item - 1].price)));
-		})
+		//Multiplies amount by item price in order to show full price
+		console.log('Total Cost: ' + (amount * parseFloat(itemData[item - 1].price)));
 	}
 }
 function itemPrompt(){
+	//Grabs the procducts table data
 connection.query('SELECT * FROM products', function(err, res){
 	if(err) throw err;
 	console.log('All items available for sale:');
 	itemData = res;
+	//displays the items in order
 	for(var i = 0; i < res.length; i++){
 		console.log('Item #' + res[i].item_id + ' -' + res[i].department_name);
 		console.log("$" + res[i].price + " " + res[i].product_name + ", " + 
 		res[i].stock_quantity + " in stock.");
 	}
+	//prompts the user to buy an item
 	inquirer.prompt([
 	{
 		name: 'itemToBuy',
